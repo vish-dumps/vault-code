63c2ee694f04c68ab616d39b4999587b97b79ee2
vishwas <vishwassoni@kgpian.iitkgp.ac.in>
2025-12-07 15:55:32 +0530

    bro plz

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const config = {
   server: {
@@ -13,13 +12,6 @@ const config = {
   },
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
-      ? [
-        await import("@replit/vite-plugin-cartographer").then((m) => m.cartographer()),
-        await import("@replit/vite-plugin-dev-banner").then((m) => m.devBanner()),
-      ]
-      : []),
   ],
   resolve: {
     alias: {
